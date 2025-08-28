e19971916a9d89fc38067c437b33cacd55e398d0
spressi <mario.reutter@gmail.com>
2025-08-28 17:45:04 +0200

    implement trigger correction for Dual Probe & split trigger correction vs. rest about EEG into two files

--- 1.2.1 EEG Markers.R
+++ 1.2.1 EEG Markers.R
@@ -1,9 +1,9 @@
 library(tidyverse)
 #source("0 General.R")
+#source("1.1 Behavior.R")
 
 writeCorrectedMarkers = F #rewrite marker file for subjects with inverted markers (low voltage = signal instead of high)
 
-# Markers -----------------------------------------------------------------
 files.eeg.markers = list.files(path.eeg.raw, pattern = ".mrk", full.names = T) %>% 
   Filter(\(x) x %>% grepl("_2", .) == F, .) %>% #get rid of second file
   Filter(\(x) x %>% grepl("_original", .) == F, .) #get rid of original marker files (just backup for transparency)
@@ -17,10 +17,10 @@ eeg.markers = eeg.markers.list %>% bind_rows(.id = "subject") %>% tibble() %>%
   filter(marker %>% grepl("Stimulus", .)) %>% 
   mutate(value = value %>% gsub("S\\s*", "", .) %>% as.integer(),
          paradigm = if_else(subject %>% grepl("a", .), "Dot Probe", "Dual Probe"))
-
 eeg.markers %>% count(subject) %>% filter(n != 1152) %>% arrange(n)
 
-#correct for inverted markers
+
+# correct for inverted markers --------------------------------------------
 invertedMarkers = eeg.markers %>% count(subject) %>% filter(n > 1152) %>% pull(subject) #some subjects had inverted markers (low voltage = signal instead of high). Every reset of the pins thus resulted in an additional marker
 #eeg.markers %>% filter(subject %in% invertedMarkers) #255 is marker stop => recode as 255 - value
 eeg.markers = eeg.markers %>% 
@@ -28,16 +28,47 @@ eeg.markers = eeg.markers %>%
   filter(value != 0) %>% #get rid of stop markers
   #check assumption: first marker is always "Mk2" (last is "Mk2304")
   #summarize(.by = subject, marker.min = first(marker), marker.max = last(marker)) %>% filter(marker.min != "Mk2=Stimulus" | marker.max != "Mk2304=Stimulus", subject %in% invertedMarkers)
-  mutate(.by = subject, trial = 1:n()) %>% 
-  #check assumption: only subjects with inverted markers are affected by wrong marker order
-  #mutate(markerCheck = paste0("Mk", trial+1, "=Stimulus")) %>% filter(markerCheck != marker) %>% pull(subject) %>% unique() %>% {. == invertedMarkers} %>% all()
-  mutate(marker = paste0("Mk", trial+1, "=Stimulus"), #this also overwrites correct markers but result has been asserted to be the same
-         output = paste(marker, paste0("S", value), sample, size, channel, sep = ","))
+  mutate(.by = subject, markerNum = 1:n()+1)
+#check assumption: only subjects with inverted markers are affected by wrong marker order
+#eeg.markers %>% mutate(markerCheck = paste0("Mk", markerNum, "=Stimulus")) %>% filter(markerCheck != marker) %>% pull(subject) %>% unique() %>% {. == invertedMarkers} %>% all()
+
+#check inverted markers
 #eeg.markers %>% filter(subject %in% invertedMarkers)
+
+
+
+# correct for Dual Probe --------------------------------------------------
+# adding 5 to markers for SOA==100 messed up everything for Dual Probe
+# targets 1 & 2 will get code 12, which will be mapped to 17 for SOA==100 => can't distinguish between 12 for SOA==100 and 17 for SOA==500
+# targets 9 & 4 will get code 94 (for angry left) - if this happens within SOA==100, we get 99, which will get confused with a correct response
+# => get rid of target information in markers and just discern between angry face position (1 vs. 2) and soa (0 vs. 10)
+
+#add trial numbers (note: don't do this before having corrected inverted markers!)
+eeg.markers2 = eeg.markers %>% 
+  filter(markerNum %% 2 == 0) %>% #first marker starts with 2 => we need all even markers
+  mutate(.by = subject, trial = 1:n()) %>% 
+  left_join(eeg.markers, .) %>% #insert trial numbers (but only for stimuli, not for responses!)
+  left_join(behavior %>% select(subject, trial, SOA)) %>% #insert SOA from behavior (only for stimuli, not for responses!)
+  
+  mutate(value = case_when(paradigm == "Dual Probe" & SOA==100 ~ value-5, #revert the +5 from SOA==100
+                           T ~ value),
+         value = case_when(paradigm == "Dual Probe" & SOA %>% is.na() == F & value < 100 ~ 1, #angry left  := 1 (i.e., drop information on targets)
+                           paradigm == "Dual Probe" & SOA %>% is.na() == F & value > 100 ~ 2, #angry right := 2 (i.e., drop information on targets)
+                           T ~ value), #leave responses and Dot Probe values untouched
+         value = case_when(paradigm == "Dual Probe" & SOA==100 ~ value+5, #now add +10 for SOA==100
+                           T ~ value)) %>% 
+  #fill(trial, .direction = "down") %>% 
+  
+  #create output that can be written into a file
+  mutate(marker = paste0("Mk", markerNum, "=Stimulus"), #this also overwrites correct markers but result has been checked to be the same
+         output = paste(marker, paste0("S", value), sample, size, channel, sep = ","))
+# check if Dot Probe has been affected
+#eeg.markers2 %>% rename(value2 = value) %>% full_join(eeg.markers) %>% filter(value!=value2) %>% pull(subject) %>% unique() %>% Filter(\(x) x %>% str_starts("a"), .)
+eeg.markers = eeg.markers2; rm(eeg.markers2)
 
 eeg.markers %>% count(subject) %>% filter(n != 1152) %>% arrange(n)
 #a07: only first block
-#a13: last trial missing in EEG
+#a13: last trial missing in EEG (note: if first trial was missing, trial numbers would need adjustment)
 
 # #check what happened to a13
 # list.files(path.seq, pattern = "a13", full.names = T) %>% Filter(\(x) x %>% grepl("_0", .) == F, .) %>% #get rid of training
@@ -46,12 +77,18 @@ eeg.markers %>% count(subject) %>% filter(n != 1152) %>% arrange(n)
 #   select(trial, condition) %>% 
 #   filter(condition != eeg.markers %>% filter(subject == "a13", value %in% c(99, 96) == F) %>% pull(value) %>% c(NA))
 # #=> no mismatch across trials
-  
+
 if (writeCorrectedMarkers) {
-  for (s in invertedMarkers) {
-    #s = sample(invertedMarkers, 1) #for testing
+  markerFilesToWrite = c(invertedMarkers, eeg.markers %>% pull(subject) %>% unique() %>% Filter(\(x) x %>% str_starts("b"), .)) %>% unique() %>% sort()
+  for (s in markerFilesToWrite) {
+    #s = sample(markerFilesToWrite, 1) #for testing
     filename = files.eeg.markers %>% Filter(\(x) x %>% grepl(s, .), .)
     filename.copy = filename %>% gsub(".vmrk", "_original.vmrk", ., fixed = T)
+    if (file.exists(filename.copy)) {
+      message(paste0(s, ": Original file already exists. Skipping creation of adjusted marker file."))
+      next
+    }
+    
     if (file.exists(filename.copy)==F) #careful! there is no option that prevents file.rename from overwriting an existing file => check yourself
       file.rename(filename, filename.copy) #this retains "last modified" as the original file creation date
     
@@ -62,45 +99,3 @@ if (writeCorrectedMarkers) {
     writeLines(file, filename)
   }
 }
-
-# Impedances --------------------------------------------------------------
-files.eeg.headers = list.files(path.eeg.raw, pattern = ".vhdr", full.names = T)
-eeg.impedances.list = list()
-for (file in files.eeg.headers) {
-  #file = files.eeg.headers %>% sample(1) #for testing
-  
-  skip = 112 #start with skipping 112 lines (b06 for whatever reason)
-  repeat { #do-while loop
-    checkFile = file %>% 
-      read_table(skip = skip, col_names = c("electrode", "impedance"), show_col_types = F, na = "???")
-    if (checkFile %>% pull(electrode) %>% grepl("Fp1", .) %>% any()) { #check if Fp1 is contained (first electrode)
-      break
-    } else {
-      skip = skip - 1
-    }
-  }
-  
-  eeg.impedances.list[[pathToCode(file)]] = checkFile
-}
-#tidy up
-eeg.impedances = eeg.impedances.list %>% bind_rows(.id = "subject") %>% 
-  mutate(electrode = electrode %>% gsub(":", "", .),
-         time = if_else(subject %>% grepl("_2", .), "after", "before") %>% as_factor(),
-         subject_session = subject,
-         subject = subject %>% gsub("_2", "", .))
-
-
-#sanity checks
-eeg.impedances %>% count(electrode) %>% count(n) %>% rename(subjects = n, electrodes = nn)
-eeg.impedances %>% filter(impedance %>% is.na()) %>% pull(subject_session) %>% unique() #TODO check NAs
-
-##impedance change before/after
-# with(eeg.impedances %>% summarize(.by = c(subject, time),
-#                              impedance = mean(impedance, na.rm=T)) %>% 
-#   pivot_wider(id_cols = subject, names_from = time, values_from = impedance),
-#   t.test(before, after, paired=T)
-# ) %>% apa::t_apa(es_ci=T)
-eeg.impedances %>% summarize(.by = c(subject, time),
-                             impedance = mean(impedance, na.rm=T))
-#just saving another file does not work to measure impedances after experiment :(
-# => need to click on impedance measurement again; otherwise previous values will just get carried over
---
+++ 1.2.2 EEG.R
@@ -0,0 +1,73 @@
+library(tidyverse)
+#source("0 General.R")
+#source("1.1 Behavior.R")
+#source("1.2.1 EEG Markers.R")
+
+# Markers -----------------------------------------------------------------
+files.eeg.markers = list.files(path.eeg.raw, pattern = ".mrk", full.names = T) %>% 
+  Filter(\(x) x %>% grepl("_2", .) == F, .) %>% #get rid of second file
+  Filter(\(x) x %>% grepl("_original", .) == F, .) #get rid of original marker files (just backup for transparency)
+eeg.markers.list = list()
+for (file in files.eeg.markers) {
+  #file = files.eeg.markers %>% sample(1) #for testing
+  eeg.markers.list[[pathToCode(file)]] = file %>% read.csv(skip=11, header=F, col.names = c("marker", "value", "sample", "size", "channel"))
+}
+#tidy up
+eeg.markers = eeg.markers.list %>% bind_rows(.id = "subject") %>% tibble() %>% 
+  filter(marker %>% grepl("Stimulus", .)) %>% 
+  mutate(value = value %>% gsub("S\\s*", "", .) %>% as.integer(),
+         paradigm = if_else(subject %>% grepl("a", .), "Dot Probe", "Dual Probe"))
+
+eeg.markers %>% count(subject) %>% filter(n != 1152) %>% arrange(n)
+#a07: only first block
+#a13: last trial missing in EEG (note: if first trial was missing, trial numbers would need adjustment)
+
+# #check what happened to a13
+# list.files(path.seq, pattern = "a13", full.names = T) %>% Filter(\(x) x %>% grepl("_0", .) == F, .) %>% #get rid of training
+#   lapply(read_tsv) %>% bind_rows() %>% 
+#   mutate(trial = 1:n(), condition = condition + if_else(soa == 100, 5, 0)) %>% 
+#   select(trial, condition) %>% 
+#   filter(condition != eeg.markers %>% filter(subject == "a13", value %in% c(99, 96) == F) %>% pull(value) %>% c(NA))
+# #=> no mismatch across trials
+  
+# Impedances --------------------------------------------------------------
+files.eeg.headers = list.files(path.eeg.raw, pattern = ".vhdr", full.names = T)
+eeg.impedances.list = list()
+for (file in files.eeg.headers) {
+  #file = files.eeg.headers %>% sample(1) #for testing
+  
+  skip = 112 #start with skipping 112 lines (b06 for whatever reason)
+  repeat { #do-while loop
+    checkFile = file %>% 
+      read_table(skip = skip, col_names = c("electrode", "impedance"), show_col_types = F, na = "???")
+    if (checkFile %>% pull(electrode) %>% grepl("Fp1", .) %>% any()) { #check if Fp1 is contained (first electrode)
+      break
+    } else {
+      skip = skip - 1
+    }
+  }
+  
+  eeg.impedances.list[[pathToCode(file)]] = checkFile
+}
+#tidy up
+eeg.impedances = eeg.impedances.list %>% bind_rows(.id = "subject") %>% 
+  mutate(electrode = electrode %>% gsub(":", "", .),
+         time = if_else(subject %>% grepl("_2", .), "after", "before") %>% as_factor(),
+         subject_session = subject,
+         subject = subject %>% gsub("_2", "", .))
+
+
+#sanity checks
+eeg.impedances %>% count(electrode) %>% count(n) %>% rename(files = n, electrodes = nn)
+eeg.impedances %>% filter(impedance %>% is.na()) %>% pull(subject_session) %>% unique() #TODO check NAs
+
+##impedance change before/after
+# with(eeg.impedances %>% summarize(.by = c(subject, time),
+#                              impedance = mean(impedance, na.rm=T)) %>% 
+#   pivot_wider(id_cols = subject, names_from = time, values_from = impedance),
+#   t.test(before, after, paired=T)
+# ) %>% apa::t_apa(es_ci=T)
+eeg.impedances %>% summarize(.by = c(subject, time),
+                             impedance = mean(impedance, na.rm=T))
+#just saving another file does not work to measure impedances after experiment :(
+# => need to click on impedance measurement again; otherwise previous values will just get carried over
